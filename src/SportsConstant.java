79b1c6a651e529bdf246fe1efcc97f4c320d8fe5
hms2274 <hms2274@an098093-ct107v.nvstu.edu>
2015-02-09 14:12:58 -0500

    hw

--- src/SportsConstant.java
+++ src/SportsConstant.java
@@ -18,13 +18,21 @@ public class SportsConstant {
 		public static final int HALF_TIME = 45;
 		public static final boolean THE_KICK_IS = true;
 		
-	public static void main(String[] args) {
 		//Basketball Constants
-		
+		public static final int FOUL_VALUE = 1;
+		public static final int THREEPOINTER = 3;
+		public static final double HALFTIME = 30.00;
+		public static final boolean WINNER = true;
 		
 		//Cricket Constant
+		public static final int PLAYER_NUMBER = 11;
+		public static final int BASE_DISTANCE = 22;
+		public static final int WINNING_RUNS = 10;
+		public static final boolean SAFE = false;
 		
 		
+	public static void main(String[] args) {
+		System.out.println("The constant values listed in the source code are fundemental rules of the commented sport.");
 		
 	}
 
