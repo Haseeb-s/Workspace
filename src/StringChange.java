830bf6f4ef30404547423cbd43f2b7b595786fbc
hms2274 <hms2274@an098093-ct107v.nvstu.edu>
2015-02-16 15:01:25 -0500

    yo

---
+++ src/StringChange.java
@@ -0,0 +1,24 @@
+
+public class StringChange {
+
+	public static void main(String[] args) {
+		String sample = "This is a sample";
+		String sample2 = "This is a second sample";
+		System.out.println("The Sample Message is " + sample);
+		System.out.println("The CharAt index 3 is" + sample.charAt(3) );
+		System.out.println(sample.compareTo(sample2));
+		System.out.println(sample.concat(sample2));
+		System.out.println(sample.equals(sample2));
+		System.out.println(sample.equalsIgnoreCase(sample));
+		System.out.println(sample.indexOf("a"));
+		System.out.println(sample.lastIndexOf("ample"));
+		System.out.println(sample.length());
+		System.out.println(sample.toLowerCase());
+		System.out.println(sample.toUpperCase());
+		System.out.println(sample.replace("I", "X"));
+		System.out.println(sample.substring(9));
+		System.out.println(sample.substring(0,6));
+		System.out.println(sample.trim());
+	}
+
+}
