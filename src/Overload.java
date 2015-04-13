3c04e8833535a3e6efc71ab2f766f03169561a66
Haseeb-s <haseebster@yahoo.com>
2015-04-13 14:37:46 -0400

    Update Overload.java

--- src/Overload.java
+++ src/Overload.java
@@ -24,4 +24,24 @@ public static double getProduct(double[] array){
 	return (total);
 }
 
+public static double getFactorial(double[] array){
+double supertotal = 0;
+int counter = array.length;
+	do{
+	double total = 1;
+	double factTotal = 1;
+	for(int x = 0; x < counter; x++){
+		factTotal=1;
+	
+		for(int y = 2; y<= array[x]; y++ )
+		{ 
+		factTotal= factTotal * y;
+		}
+	}
+
+	supertotal += factTotal;
+	counter--;
+	}while(counter > 0);
+	return (supertotal);
+}
 }
