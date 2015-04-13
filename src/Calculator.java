abb486822208a95380c21c23b411d9852446d5a2
Haseeb-s <haseebster@yahoo.com>
2015-04-13 14:37:37 -0400

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,7 +3,7 @@ public class Calculator {
 
 	public static void main(String[] args) {
 	Overload calc = new Overload();
-	//sum = 21, average = 3.5, product = 720
+	//sum = 21, average = 3.5, product = 720, factorial = 873
 	double[] array = {1,2,3,4,5,6};
 	
 	System.out.println("Sum is : " + calc.getSum(array));
@@ -11,5 +11,7 @@ public class Calculator {
 	System.out.println("Average is : " + calc.getAverage(array));
 
 	System.out.println("Product is : " +calc.getProduct(array));
+	
+	System.out.println("Factorial is : " +calc.getFactorial(array));
 	}
 }
