5442c6b68e3365a28735126e55cd98312dc2e879
Haseeb-s <haseebster@yahoo.com>
2015-04-20 11:58:24 -0400

    Create SwingGridLayout.java

---
+++ src/Layout/SwingGridLayout.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+public class SwingGridLayout extends JFrame{
+	
+	private JButton jbutt[] = new JButton[10];
+	private JTextField[] field1 = new JTextField[1];
+	
+	public SwingGridLayout()	{
+		
+		jbutt[1] = new JButton("1");
+		jbutt[2] = new JButton("2");
+		jbutt[3] = new JButton("3");
+		jbutt[4] = new JButton("4");
+		
+		
+		setLayout(new GridLayout(2,2));
+		
+		add(jbutt[1]);
+		add(jbutt[2]);
+		add(jbutt[3]);
+		add(jbutt[4]);
+		field1[0] = new JTextField(40);
+		add(field1[0]);
+		setVisible(true);
+		setSize(new Dimension(300,400));
+		}
+}
