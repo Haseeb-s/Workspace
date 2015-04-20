31017577a19ab02402d561684ca7226bf4594f92
Haseeb-s <haseebster@yahoo.com>
2015-04-20 11:58:48 -0400

    Create SwingJavaLayoutDriver.java

---
+++ src/Layout/SwingJavaLayoutDriver.java
@@ -0,0 +1,12 @@
+
+public class SwingJavaLayoutDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SwingFlowLayout gui = new SwingFlowLayout();
+		SwingGridLayout gui1 = new SwingGridLayout();
+		SwingBorderLayout gui2 = new SwingBorderLayout();
+		
+	}
+
+}
