f314d91e6878ac39978cbaf4efb7b8ee1e871bc2
Haseeb-s <haseebster@yahoo.com>
2015-06-23 23:34:57 -0400

    Update Cycle.java

--- src/Cycle.java
+++ src/Cycle.java
@@ -1,1 +1,24 @@
 
+public class Cycle {
+	private int numberOfWheels;
+	private int weight;
+	
+	public Cycle(){
+		Cycle cycle = new Cycle(100,1000);
+		this.numberOfWheels = cycle.numberOfWheels;
+		this.weight = cycle.weight;
+	}
+	public Cycle(int numberOfWheels, int weight){
+		this.numberOfWheels = numberOfWheels;
+		this.weight = weight;
+	}
+	
+	
+	public String toString(){
+		return "The number of wheels is: "
+				+ numberOfWheels
+				+ "\nThe weight is " + weight;
+	}
+
+	
+}
