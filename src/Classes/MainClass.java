8c09162804633350ac0d000ce887d38c267d4066
Haseeb-s <haseebster@yahoo.com>
2015-07-18 16:31:10 -0400

    Create MainClass.java

---
+++ src/Classes/MainClass.java
@@ -0,0 +1,13 @@
+
+public class MainClass {
+public static void main(String args[]){
+	ComputerScience CSC201 = new ComputerScience("Tanes","201",20,"Learn and Master OOP", "201C", 45);
+	MathClass MTH174 = new MathClass("Smith","174",25,"Calc II", "301", 75, 4, 4.0);
+	PEDClass PED101 = new PEDClass("Joe", "PED 113", 30, 9001, 00, "Today I ate nothing");
+	System.out.println(CSC201);
+	System.out.println("");
+	System.out.println(MTH174);
+	System.out.println("");
+	System.out.println(PED101);
+}
+}
