3eda60f7cf2fe666d0ed802dbff5972ce011cf6e
Haseeb-s <haseebster@yahoo.com>
2015-07-18 14:36:01 -0400

    Create SportMain.java

---
+++ src/SportStats/SportMain.java
@@ -0,0 +1,11 @@
+
+public class SportMain {
+	
+	public static void main(String args[]){
+		Basketball testBall = new Basketball("Chicago Bulls",7,3,50,15);
+		Hockey penguins = new Hockey("Penguins",11,4,20,5,3,75);
+		System.out.println(testBall);
+		System.out.println("\n" + penguins);
+		}
+	
+}
