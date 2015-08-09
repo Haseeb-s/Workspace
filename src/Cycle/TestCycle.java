910228bcec507390f7246f782975662e4360eb08
Haseeb-s <haseebster@yahoo.com>
2015-08-09 12:50:38 -0400

    Update TestCycle.java

--- src/Cycle/TestCycle.java
+++ src/Cycle/TestCycle.java
@@ -4,19 +4,22 @@ import java.util.Scanner;
 public class TestCycle {
 
     public static void main(String[] args) {
-        double numberOfWheels = 0;
-        double weight = 0;
-        try{
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter value for numberOfWheels");
-        numberOfWheels = scanner.nextDouble();
-        System.out.println("Enter value for weight");
-        weight = scanner.nextDouble();}
-        catch (InputMismatchException e){
-            System.out.println("INVALID INPUT");
-        }
-        Cycle cycleA = new Cycle(numberOfWheels,weight);
-        System.out.println(cycleA);
+        boolean repeat = true;
+        do {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter value for numberOfWheels");
+                double numberOfWheels = scanner.nextDouble();
+                System.out.println("Enter value for weight");
+                double weight = scanner.nextDouble();
+                Cycle cycleA = new Cycle(numberOfWheels, weight);
+                if(numberOfWheels>0&&weight>0){
+                repeat = false;
+                System.out.println(cycleA);}
+            } catch (InputMismatchException e) {
+                System.out.println("INVALID INPUT");
+            }
+        } while (repeat);
 
         //B
         System.out.println("PART B");
