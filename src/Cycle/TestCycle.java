007570b13311f718aab35ab6aed36d3818d151dc
Haseeb-s <haseebster@yahoo.com>
2015-08-09 12:35:28 -0400

    Create TestCycle.java

---
+++ src/Cycle/TestCycle.java
@@ -0,0 +1,14 @@
+
+public class TestCycle {
+
+    public static void main(String[] args) {
+        Cycle cycle = new Cycle(40,400);
+        System.out.println(cycle);
+
+        //B
+        System.out.println("PART B");
+        Cycle cycleB = new Cycle();
+        System.out.println(cycleB);
+    }
+
+}
