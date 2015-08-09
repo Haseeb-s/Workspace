ef33ae1ec2f399997b88cb33d343085679ffd591
Haseeb-s <haseebster@yahoo.com>
2015-08-09 12:50:50 -0400

    Update Cycle.java

--- src/Cycle/Cycle.java
+++ src/Cycle/Cycle.java
@@ -1,27 +1,22 @@
 import java.io.IOException;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+        import java.util.InputMismatchException;
+        import java.util.Scanner;
 
 public class Cycle {
     private double numberOfWheels;
     private double weight;
 
     public Cycle() {
-        boolean pass = true;
-        do {
-            try {
-                if (this.weight < 0 || this.numberOfWheels < 0) {
-                    IOException x = new IOException();
-                    throw x;
-                }
-                else
-                    pass = false;
-            } catch(IOException e) {
-                System.out.println("Incorrect input!");
+
+        try {
+            if (this.weight < 0 || this.numberOfWheels < 0) {
+                IOException x = new IOException();
+                throw x;
             }
 
-
-        } while (pass);
+        } catch (IOException e) {
+            System.out.println("Incorrect input!");
+        }
     }
 
     public Cycle(double numberOfWheels, double weight) {
