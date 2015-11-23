a55c2394ed3ce4c7021eb2b82a0cf680fb7404a2
Haseeb-s <haseebster@yahoo.com>
2015-11-23 16:23:14 -0500

    Create Vertex.java

---
+++ src/realAssignment5/Vertex.java
@@ -0,0 +1,30 @@
+public class Vertex implements Comparable<Vertex> {
+    public String name;
+
+    public int distance;
+    public Vertex predecessor; // previous vertex
+    public double centrality;
+    public static final int INFINITY = Integer.MAX_VALUE;
+
+    public Vertex(String v)
+    {
+        name = v;
+        distance = INFINITY; // start as infinity away
+        predecessor = null;
+        centrality = 0.0;
+    }
+
+    public String toString()
+    {
+        return name;
+    }
+
+    public int compareTo(Vertex other)
+    {
+        int diff = distance - other.distance;
+        if (diff != 0)
+            return diff;
+        else
+            return name.compareTo(other.name);
+    }
+}
